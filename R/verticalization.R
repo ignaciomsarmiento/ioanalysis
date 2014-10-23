df7aa8787c18793cd2e7d3164d9ac30001624d24
ignaciomsarmiento <ignaciomsarmiento@gmail.com>
2014-10-23 14:11:39 -0500

    minor changes in the function

--- R/verticalization.R
+++ R/verticalization.R
@@ -5,14 +5,13 @@ verticalization<-function(IMP,X,E,namesector, write.xlsx=TRUE){
   df
 }
 
-vert.matrix<-function(IMP,X, write.xlsx=TRUE){
+vert.matrix<-function(IMP,X, write.xlsx=TRUE, name="Level_Verticalization.xlsx"){
   if(class(IMP)!="matrix")stop("Should be element of a matrix class")
   if(class(X)!="matrix")stop("Should be element of a matrix class")
-
   nosect<-dim(X)[1]
   u<-rbind(rep(1,nosect))
-  x.i<-cbind(apply(X,1,sum))
-  vert<-(t(u %*% A %*% X))/x.i 
-  if(write.xlsx==TRUE)write.xlsx(vert, file="Level_Verticalization.xlsx", sheetName="Sheet1", showNA=TRUE)
+  x.i<-rowSums(X)
+  vert<-(u %*%t(A) %*% X)/x.i 
+  if(write.xlsx==TRUE)write.xlsx(vert, file=name, sheetName="Sheet1", showNA=TRUE)
   vert
 }
