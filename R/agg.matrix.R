d84df1cee490b2fb67017142f3caf3189ef72f4c
ignaciomsarmiento <ignaciomsarmiento@gmail.com>
2014-10-27 20:48:08 -0500

    minor changes

--- R/agg.matrix.R
+++ R/agg.matrix.R
@@ -1,6 +1,6 @@
-agg.matrix<-function(mat,d.mip){
+agg.matrix<-function(mat,dimcol){
   mat[is.na(mat)] <- 0
-  s<-array(0,c(dim(mat)[1],d.imp))
+  s<-array(0,c(dim(mat)[1],dimcol))
   for (i in 1:nrow(mat)){
     for (j in 2:ncol(mat)){
       if(max(mat[i,j])!=0){s[i,max(mat[i,j])]<-1}
