dbb1053304d48c59f5ea85210a4b7f21fbb51a7a
ignaciomsarmiento <ignaciomsarmiento@gmail.com>
2014-11-19 09:02:52 -0600

    Changed the vs function and updated the help file

--- R/vs.io.R
+++ R/vs.io.R
@@ -1,11 +1,10 @@
-vs.io<-function(imp,exp,namesector, write.xlsx=TRUE, name="Level_Verticalization.xlsx"){
-  if(dim(imp)[2]!=dim(exp)[1])stop("check dimensions")
-  ax<-imp%*%exp
+vs.io<-function(imp,exp,namesector,leon=1, write.xlsx=TRUE, name="Level_Verticalization.xlsx"){
+  if(length(leon)==1){ax<-as.matrix(imp)%*%as.matrix(exp)}
+  else{ax<-as.matrix(imp)%*%as.matrix(leon)%*%as.matrix(exp)}
   xk<-sum(exp)
   vert<-ax/xk
   df <-data.frame(sectors = c(namesector),  vs=c(ax), exports=c(exp), level_verticalization = c(vert)) 
   format(df, digits=2, nsmall=2, scientific = FALSE)
   if(write.xlsx==TRUE)write.xlsx(df, file=name, sheetName="Sheet1", showNA=TRUE)
-  list(format(df, digits=2, nsmall=2, scientific = FALSE), total.exp=(format(sum(exp), scientific = FALSE)))
+  list(format(df, digits=2, nsmall=2, scientific = FALSE), total.exp=(format(sum(exp), scientific = FALSE)), vs=(format(sum(ax)/xk, scientific = FALSE)))
 }
-
