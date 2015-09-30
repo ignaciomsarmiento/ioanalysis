72cee91c28b16ef14b0f58cae9cf8959ea4b9c6c
ignaciomsarmiento <ignaciomsarmiento@gmail.com>
2015-09-30 09:16:49 -0500

    fixed upstream function and manuals

--- R/upstream.R
+++ R/upstream.R
@@ -1,9 +1,12 @@
-upstream<-function(mip,y,x,m, write.xlsx=TRUE, name="Upstream.xlsx"){
-  if(class(mip)!="matrix") mip<-as.matrix(mip)
-  nosect<-dim(mip)[1]
-  mip<-(mip/y)
+upstream<-function(A,y,x,m, write.xlsx=TRUE, name="Upstream.xlsx"){
+  if(class(A)!="matrix") A<-as.matrix(A)
+  if(class(y)!="numeric") y<-as.numeric(y)
+  if(class(x)!="numeric") x<-as.numeric(x)
+  if(class(m)!="numeric") m<-as.numeric(m)
+  nosect<-dim(A)[1]
+  A<-(A/y)
   w<-y/(y-x+m)
-  dij<-mip*w
+  dij<-A*w
   d.inv<-solve(diag(nosect)-dij)
   u<-cbind(rep(1,nosect))
   U<-d.inv%*%u
